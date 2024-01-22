0ef7938253e182db6ad55e857007d89122cd348c
Adarshowl <pmowlok@gmail.com>
2024-01-22 19:48:05 +0530

    22 jan code push1

---
+++ src/constants/images.js
@@ -0,0 +1,30 @@
+export const bg_no_item_cactus = require('../assets/images/bg_no_item_cactus.png');
+export const splash = require('../assets/images/seller.jpg');
+// export const app_logo = require('../assets/images/seller_logo.jpg');
+export const app_logo = require('../assets/images/app_logo.png');
+export const intro_a = require('..//assets/images/intro/vendor1.png');
+export const intro_b = require('../assets/images/intro/vendor2.png');
+export const intro_c = require('../assets/images/intro/vendor3.png');
+export const offer_bg = require('../assets/images/offer_bg.jpg');
+export const filter = require('../assets/images/filter.png')
+export const address = require('../assets/images/address.jpg');
+export const promo = require('../assets/images/promo1.jpg')
+export const forgot = require('../assets/images/forgot.jpg')
+export const new_splash = require('../assets/images/adp_Splash.jpg')
+
+
+
+export default {
+  new_splash,
+  bg_no_item_cactus,
+  splash,
+  intro_a,
+  intro_b,
+  intro_c,
+  app_logo,
+  offer_bg,
+  filter,
+  address,
+  promo,
+  forgot
+};
